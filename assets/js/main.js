ab65857b39c29bcb9e4e0f70c41bbbe87a8c194a
Felipe Melo <felipemelogomes36@gmail.com>
2023-06-27 14:19:58 -0300

    fix: css clean code

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
     const navMenu = document.getElementById("nav-menu");
-    // When we click on each nav__link, we remove the show-menu class
+
     navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -37,7 +37,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const blurHeader = () => {
     const header = document.getElementById("header");
-    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+
     this.scrollY >= 50
         ? header.classList.add("blur-header")
         : header.classList.remove("blur-header");
@@ -87,7 +87,7 @@ contactForm.addEventListener("submit", sendEmail);
 
 const scrollUp = () => {
     const scrollUp = document.getElementById("scroll-up");
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+
     this.scrollY >= 350
         ? scrollUp.classList.add("show-scroll")
         : scrollUp.classList.remove("show-scroll");
