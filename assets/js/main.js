b78b74b4193e6abfe52d30830b36e2783bc8de75
Felipe Melo <felipemelogomes36@gmail.com>
2023-09-27 16:28:42 -0300

    feat: animacao js melhorada

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,7 +133,7 @@ sr.reveal(
 sr.reveal(".home__image", { origin: "bottom" });
 sr.reveal(".about__data, .skills__data", { origin: "left" });
 sr.reveal(".about__image, .skills__content", { origin: "right" });
-sr.reveal(".services__card, .projects__card", { interval: 50 });
+sr.reveal(".services__card, .projects__card", { interval: 200 });
 
 /* ANIMATION DIGITATION */
 document.addEventListener("DOMContentLoaded", () => {
